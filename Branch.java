6297afd30dffa98b86896b4784df7284eb88213d
AlexandrosArabatzis <64361373+AlexandrosArabatzis@users.noreply.github.com>
2021-03-08 03:11:35 +0100

    Update Branch.java

--- Branch.java
+++ Branch.java
@@ -1,28 +1,3 @@
-
-// You job is to create a simple banking application.
-// There should be a Bank class
-// It should have an arraylist of Branches
-// Each Branch should have an arraylist of Customers
-// The Customer class should have an arraylist of Doubles (transactions)
-// Customer:
-// Name, and the ArrayList of doubles.
-// Branch:
-// Need to be able to add a new customer and initial transaction amount.
-// Also needs to add additional transactions for that customer/branch
-// Bank:
-// Add a new branch
-// Add a customer to that branch with initial transaction
-// Add a transaction for an existing customer for that branch
-// Show a list of customers for a particular branch and optionally a list
-// of their transactions
-// Demonstration autoboxing and unboxing in your code
-// Hint: Transactions
-// Add data validation.
-// e.g. check if exists, or does not exist, etc.
-// Think about where you are adding the code to perform certain actions
-
-package arabatzis;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -63,6 +38,8 @@ public class Branch {
 		return false;
 	}
 
+	
+	// check if a customer already exists. If yes, return the customer object. If not, return null.
 	private Customer findCustomer(String customerName) {
 		for (int i = 0; i < this.customers.size(); i++) {
 			Customer checkedCustomer = this.customers.get(i);
